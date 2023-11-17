0bc8d7175ad69e0652db4c3bc313b26c92630971
dom-io <111844962+dom-io@users.noreply.github.com>
2023-11-17 01:19:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var m
+}
